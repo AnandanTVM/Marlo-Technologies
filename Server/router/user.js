be8bdae77cf028ff5292fb015593ab2107903a80
anandan <anandan2006@gmail.com>
2023-03-08 20:42:33 +0530

    token verification done

--- Server/router/user.js
+++ Server/router/user.js
@@ -1,8 +1,12 @@
 // user route
 const express = require("express");
 const userControllers = require("../controller/userController");
+const userAuth = require("../middleware/auth");
 const router = express.Router();
 // login route
 router.post("/login", userControllers.userLoginContro);
-
+router.get("/profile", userAuth.userProtect, (req, res) => {
+  console.log("token verification done");
+  res.json({ user: req.user });
+});
 module.exports = router;
--- Server/util/userUtil.js
+++ Server/util/userUtil.js
@@ -1,6 +1,7 @@
 const collection = require("../config/collection");
 const db = require("../config/connection");
 const bcrypt = require("bcrypt");
+const { ObjectId } = require("mongodb");
 module.exports = {
   loginUser: (details) =>
     new Promise((resolve, reject) => {
@@ -30,6 +31,14 @@ module.exports = {
             reject({ message: "No user found..", status: 404 });
           }
         })
+        .catch((err) => reject(err));
+    }),
+  findUserById: (Id) =>
+    new Promise((resolve, reject) => {
+      db.get()
+        .collection(collection.USER_COLLECTION)
+        .findOne({ _id: ObjectId(Id) })
+        .then((user) => resolve(user))
         .catch((err) => reject(err));
     }),
 };
